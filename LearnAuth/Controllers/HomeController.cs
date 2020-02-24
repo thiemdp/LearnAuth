e28098be2fb384cfe0c1dab9015c507cf1a6b21e
Thiemdp <thiemdp.ekgis@gmail.com>
2020-02-24 20:42:53 +0700

    Add Authentication menthod

--- LearnAuth/Controllers/HomeController.cs
+++ LearnAuth/Controllers/HomeController.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -22,6 +24,15 @@ namespace LearnAuth.Controllers
 
         public IActionResult Authenticate()
         {
+            var claims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Name ,"thiemdp"),
+                new Claim(ClaimTypes.Email,"thiemdp.ekgis@gmail.com")
+            };
+            var idtt = new ClaimsIdentity(claims, "thiemdp identity");
+            var userprincipal = new ClaimsPrincipal(new[] { idtt });
+            HttpContext.SignInAsync(userprincipal);
+
            return RedirectToAction("Index");
         }
     }
