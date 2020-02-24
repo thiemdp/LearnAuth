be925b3e8a8289935612c8b6ddd50dd6c23b44bc
Thiemdp <thiemdp.ekgis@gmail.com>
2020-02-24 14:13:47 +0700

    Add project files.

---
+++ LearnAuth/Startup.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace LearnAuth
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Thiết lập cách xác thực
+            services.AddAuthentication("CookieAuth")
+                .AddCookie("CookieAuth", config=> {
+                config.Cookie.Name = "Nanocell.Cookie";
+                config.LoginPath = "/home/authenticate";
+            });
+
+
+            services.AddControllersWithViews();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+            // xác minh xem bạn là ai?
+            app.UseAuthentication();
+            // xác minh xem bạn được làm gì trong hệ thống.
+            // lưu ý: authen luôn phải chạy trước autho.
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapDefaultControllerRoute();
+            });
+        }
+    }
+}
